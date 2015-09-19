a125b8a9da99b06de9556fccb0c06505b54d995d
ilkinunal <ilkin.unal@att.com>
2015-09-19 17:02:16 -0400

    updates

--- templates.py
+++ templates.py
@@ -1,11 +1,12 @@
 import webapp2
 import jinja2
 import os
+import time
+from google.appengine.ext import ndb
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+
+template_dir = os.path.join(os.path.dirname(__file__), 'notes')
+JINJA_ENVIRONMENT = jinja2.Environment( loader=jinja2.FileSystemLoader(template_dir), extensions=['jinja2.ext.autoescape'], autoescape=True)
 
 class Handler(webapp2.RequestHandler): 
     """
@@ -16,7 +17,7 @@ class Handler(webapp2.RequestHandler):
         self.response.out.write(*a, **kw)  
 
     def render_str(self, template, **params):  
-        "Render jija2 templates"
+        "Render jinja2 templates"
         t = JINJA_ENVIRONMENT.get_template(template)
         return t.render(params)   
     
@@ -28,8 +29,35 @@ class MainHandler(Handler):
     def get(self):
         self.render("Notes-4.html")
 
-app = webapp2.WSGIApplication([
-    ('/notes', MainHandler)
-], debug=True)
+class UserComment(ndb.Model):
+    user = ndb.StringProperty()
+    comment = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
+class MainPageComments(webapp2.RequestHandler):
+    def get(self):
+        error = self.request.get('error','')
+        query = UserComment.query().order(UserComment.date)
+        usercomment_list = query.fetch(10)
+
+        for usercomment in query:
+            user = usercomment.user
+            comment = usercomment.comment
+    self.response.out.write(user, comment)
+
+    def post(self):
+        user = self.request.get('user')
+        comment = self.request.get('comment')
+
+        if user and comment:
+            usercomment = UserComment(user=user, comment=comment)
+            usercomment.put()
+            time.sleep(.1)
+            self.redirect('/')
+        else:
+            self.redirect('/?error=Please fill out the name and comment sections!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler), ('/', MainPageComments)
+], debug=True)
 
